63642e0b3be2057aeb0489f30e855b374abd77fd
francescapetra <francescalaurapetra@gmail.com>
2021-11-23 15:52:05 +0100

    add-middleware

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,10 @@ use PhpParser\Node\Stmt\TryCatch;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     /**
      * Display a listing of the resource.
      *
